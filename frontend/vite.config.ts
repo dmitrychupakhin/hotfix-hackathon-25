b34f4db8eeec47f1362d1e20cb0b6ef286208b09
dmitrychupakhin <chupahkin@gmail.com>
2025-05-30 12:47:33 +0300

    chore: init react app, init svgr, alias, init eslint config + eslint stylistic

---
+++ frontend/vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import svgr from 'vite-plugin-svgr'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), svgr()],
+  build: {
+    assetsInlineLimit: 0,
+  },
+  resolve: {
+    alias: [{ find: '@', replacement: '/src' }],
+  },
+  define: {
+    __API__: JSON.stringify('https://crocodailo.ru/api'),
+  },
+})
