edebdd9024f645e4459bee6ecb014fb05ac90ecd
dmitrychupakhin <chupahkin@gmail.com>
2025-05-31 01:49:34 +0300

    feat: udpate home hero

--- frontend/src/pages/Home/ui/Hero/HomeHero.tsx
+++ frontend/src/pages/Home/ui/Hero/HomeHero.tsx
@@ -23,68 +23,6 @@ const HomeHero = () => {
         height: 'auto',
       }}
       />
-      <Box sx={{
-        position: 'absolute',
-        bottom: 0,
-        right: 0,
-        zIndex: 2,
-        p: 4,
-      }}
-      >
-        <Stack spacing={2} direction="row" alignItems="center">
-          <Stack spacing={1} direction="row" alignItems="center">
-            <Box sx={{
-              borderRadius: '50%',
-              backgroundColor: 'primary.main',
-              aspectRatio: 1,
-              height: '42px',
-              width: '42px',
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'center',
-              fontWeight: 600,
-            }}
-            >
-              5
-            </Box>
-            <Typography fontWeight={600}>Сотрудников</Typography>
-          </Stack>
-          <Stack spacing={1} direction="row" alignItems="center">
-            <Box sx={{
-              borderRadius: '50%',
-              backgroundColor: 'primary.main',
-              aspectRatio: 1,
-              height: '42px',
-              width: '42px',
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'center',
-              fontWeight: 600,
-            }}
-            >
-              3
-            </Box>
-            <Typography fontWeight={600}>Проекта</Typography>
-          </Stack>
-          <Stack spacing={1} direction="row" alignItems="center">
-            <Box sx={{
-              borderRadius: '50%',
-              backgroundColor: 'primary.main',
-              aspectRatio: 1,
-              height: '42px',
-              width: '42px',
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'center',
-              fontWeight: 600,
-            }}
-            >
-              0
-            </Box>
-            <Typography fontWeight={600}>Багов</Typography>
-          </Stack>
-        </Stack>
-      </Box>
       <AnimatePresence>
         <MotionBox
           initial={{ opacity: 0, y: 50 }}
@@ -99,6 +37,68 @@ const HomeHero = () => {
             height: 'auto',
           }}
         >
+          <Box sx={{
+            position: 'absolute',
+            bottom: 0,
+            right: 0,
+            zIndex: 2,
+            p: 4,
+          }}
+          >
+            <Stack spacing={2} direction="row" alignItems="center">
+              <Stack spacing={1} direction="row" alignItems="center">
+                <Box sx={{
+                  borderRadius: '50%',
+                  backgroundColor: 'primary.main',
+                  aspectRatio: 1,
+                  height: '42px',
+                  width: '42px',
+                  display: 'flex',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  fontWeight: 600,
+                }}
+                >
+                  5
+                </Box>
+                <Typography fontWeight={600}>Сотрудников</Typography>
+              </Stack>
+              <Stack spacing={1} direction="row" alignItems="center">
+                <Box sx={{
+                  borderRadius: '50%',
+                  backgroundColor: 'primary.main',
+                  aspectRatio: 1,
+                  height: '42px',
+                  width: '42px',
+                  display: 'flex',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  fontWeight: 600,
+                }}
+                >
+                  3
+                </Box>
+                <Typography fontWeight={600}>Проекта</Typography>
+              </Stack>
+              <Stack spacing={1} direction="row" alignItems="center">
+                <Box sx={{
+                  borderRadius: '50%',
+                  backgroundColor: 'primary.main',
+                  aspectRatio: 1,
+                  height: '42px',
+                  width: '42px',
+                  display: 'flex',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  fontWeight: 600,
+                }}
+                >
+                  0
+                </Box>
+                <Typography fontWeight={600}>Багов</Typography>
+              </Stack>
+            </Stack>
+          </Box>
           <ImgCornerVector style={{
             transform: 'rotate(180deg)',
           }}
